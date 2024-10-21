fdbd1515ae094f782dc7e8c64b15b875bf784505
Vladislav Parfeniuc <v.parfeniuc@hastega.it>
2024-10-21 19:10:01 +0200

    changed shell scripts

--- shell/promptorium.bash
+++ shell/promptorium.bash
@@ -2,10 +2,8 @@
 function prompt_cmd() {
     # Keep this line at the top of the function, otherwise the exit code will get
     local exit_code="$?"
-    local promptorium_config_path="$HOME/.config/promptorium/conf.json"
-    local promptorium_theme_path="$HOME/.config/promptorium/theme.json"
     local promptorium_output
-    promptorium_output=$(promptorium prompt --config-file "$promptorium_config_path" --theme-file "$promptorium_theme_path" --shell bash --exit-code "$exit_code")
+    promptorium_output=$(promptorium prompt --shell bash --exit-code "$exit_code")
     PS1="$promptorium_output"
 }
 
